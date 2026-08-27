6b5d7a2630626ccf0b85c6229ed14dc1ba838b4f
Shaik Shoyab <132129021+ShaikShoyab786@users.noreply.github.com>
2026-08-27 18:25:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ birthdays_dict = {(data_row["month"], data_row["day"]):data_row for (index, data
 
 if today_tuple in birthdays_dict:
     birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter format/letter_{random.randint(1,3)}.txt"
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
     with open(file_path) as letter_file:
         contents = letter_file.read()
         contents =contents.replace("[Name]", birthday_person["name"])
