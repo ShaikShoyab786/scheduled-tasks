884413b8792157b61325048fe0bb7e0c02f220a7
Shaik Shoyab <132129021+ShaikShoyab786@users.noreply.github.com>
2026-08-27 18:23:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ my_email_password = "wunnulkmwxtenwtl"
 today = dt.datetime.now()
 today_tuple = (today.month, today.day)
 
-data = pandas.read_csv("birthday_wishes_data.csv")
+data = pandas.read_csv("birthdays.csv")
 birthdays_dict = {(data_row["month"], data_row["day"]):data_row for (index, data_row) in data.iterrows()}
 
 if today_tuple in birthdays_dict:
